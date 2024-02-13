1eafd49221089fb2054180c7ab1021ded275e9ce
kritika_modi <109297741+9818kritika@users.noreply.github.com>
2024-02-13 12:34:09 +0530

    Add files via upload

---
+++ Vertix logistics/script.js
@@ -0,0 +1,7 @@
+alert("Compatiblity test");
+prompt("enter your name");
+prompt("enter your crush name");
+var lovescore = Math.random();
+lovescore = lovescore * 100;
+lovescore = Math.floor(lovescore) + 1;
+alert("you love score is " + lovescore + "%");
